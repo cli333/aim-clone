71515217b163e4d4f90b0536ab8f2617819275f4
cli333 <cml8198@protonmail.com>
2020-04-01 11:20:07 -0700

    added auth route

--- client/schema/schema.sql
+++ client/schema/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users
 (
   id SERIAL,
   screenname VARCHAR(50) NOT NULL,
-  password VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   friends VARCHAR(50)
   [],
 	UNIQUE
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { Client } = require("pg");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const elephantConnectionString =
   "postgres://sjjcnsze:x5YGKeKksOXyIg0btWB-bT16IKqhvjR2@drona.db.elephantsql.com:5432/sjjcnsze";
 const herokuConnectionString =
-  "postgres://qjyymapwpnobtn:4eab2931806cd6a51594358693dd4550da5297a129202cdbdc3abdb9d7c47545@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d80rqsso6qdia2";
+  "postgres://yezmhlwafbhdvi:aa754df7fbf73d912bd098a6b5357b3c377c1594d18fb3082579f095d73988ee@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dadfi09fa6c8mf";
 const client = new Client({
   connectionString: herokuConnectionString,
   ssl: true
@@ -16,12 +17,50 @@ client.connect();
 
 router.post("/", (req, res) => {
   const { screenName, password } = req.body;
-  const query = "SELECT NOW()";
-  client.query(query, (err, result) => {
-    if (err) throw err;
-    res.json(result);
-    client.end();
-  });
+  client.query(
+    "SELECT * FROM users WHERE screenname = $1 LIMIT 1",
+    [screenName],
+    (err, result) => {
+      if (err) {
+        throw err;
+      } else if (result.rows.length === 0) {
+        bcrypt.hash(password, 10, (err, hash) => {
+          if (err) {
+            throw err;
+          } else {
+            client.query(
+              "INSERT INTO users(screenname, password) VALUES($1, $2)",
+              [screenName, hash],
+              (err, result) => {
+                if (err) {
+                  throw err;
+                } else {
+                  jwt.sign({ screenName }, "secretkey", (err, token) => {
+                    res.json({ token });
+                  });
+                }
+              }
+            );
+          }
+        });
+      } else {
+        const hash = result.rows[0].password;
+        bcrypt.compare(password, hash, (err, result) => {
+          if (err) {
+            throw err;
+          } else {
+            if (result === true) {
+              jwt.sign({ screenName }, "secretkey", (err, token) => {
+                res.json({ token });
+              });
+            } else {
+              res.json("Incorrect password");
+            }
+          }
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
