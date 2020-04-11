1435015321c1e370422da67b7b0cf591ec01f82a
cli333 <cml8198@protonmail.com>
2020-04-11 14:50:05 -0700

    update package.json

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const colors = require("colors");
 const redisClient = require("./redisClient/client");
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -41,6 +42,13 @@ io.on("connection", (socket) => {
     console.log("a user disconnected".brightRed.underline);
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 server.listen(port, () =>
   console.log(`server listening on port ${port}`.rainbow.bold)
