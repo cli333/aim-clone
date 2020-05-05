4d240a1526a5c95e59784155a50ad017f6b19d94
cli333 <50061720+cli333@users.noreply.github.com>
2020-05-04 19:14:41 -0700

    Update clients.js

--- redisPg/clients.js
+++ redisPg/clients.js
@@ -9,7 +9,7 @@ const herokuConnectionString =
   "postgres://yezmhlwafbhdvi:aa754df7fbf73d912bd098a6b5357b3c377c1594d18fb3082579f095d73988ee@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dadfi09fa6c8mf";
 
 const pgClient = new Client({
-  connectionString: herokuConnectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
