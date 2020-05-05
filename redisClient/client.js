032de2725afd0011708d1db399c1fc04686d5498
cli333 <50061720+cli333@users.noreply.github.com>
2020-05-04 19:12:45 -0700

    Update client.js

--- redisClient/client.js
+++ redisClient/client.js
@@ -1,7 +1,7 @@
 const redis = require("redis");
 
 const client = redis.createClient(
-  "redis://h:pbd0ea46cc29cb044195493a883b6e4f63c50e998424bbe086ca61c080cf6a0f4@ec2-52-86-253-161.compute-1.amazonaws.com:25209"
+  process.env.REDIS_URI
 );
 
 module.exports = client;
