ff2edd9cc5291443519ca4037238d8d887a594db
cli333 <50061720+cli333@users.noreply.github.com>
2020-05-04 19:15:18 -0700

    Update clients.js

--- redisPg/clients.js
+++ redisPg/clients.js
@@ -18,7 +18,7 @@ pgClient.connect();
 // redis client
 
 const redisClient = redis.createClient(
-  "redis://h:pbd0ea46cc29cb044195493a883b6e4f63c50e998424bbe086ca61c080cf6a0f4@ec2-52-86-253-161.compute-1.amazonaws.com:25209"
+  process.env.REDIS_URL
 );
 
 module.exports = { pgClient, redisClient };
